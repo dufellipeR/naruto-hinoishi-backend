4e53126d87861aa2fe1584c06f7b463b5db2d5b7
dufellipeR <eduardo.fellipe30@gmail.com>
2021-01-03 20:34:11 -0300

    adding character routes to index

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,8 @@
+import charactersRouter from '@modules/characters/infra/http/routes/characters.routes';
 import { Router } from 'express';
 
 const routes = Router();
+
+routes.use('/character', charactersRouter);
 
 export default routes;
