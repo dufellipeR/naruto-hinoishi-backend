58ea7ff5f7c292b7f87afbc87766589415f53689
dufellipeR <eduardo.fellipe30@gmail.com>
2021-01-03 20:30:42 -0300

    Creating characters router

---
+++ src/modules/characters/infra/http/routes/characters.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+// import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import CharactersController from '../controllers/CharactersController';
+
+const charactersRouter = Router();
+const charactersController = new CharactersController();
+
+// charactersRouter.use(ensureAuthenticated);
+
+charactersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      thumbnail: Joi.string(),
+      type: Joi.string().required(),
+      name: Joi.string().required(),
+      desc: Joi.string().required(),
+      power: Joi.number().required(),
+      intelligence: Joi.number().required(),
+      speed: Joi.number().required(),
+      taijutsu: Joi.number().required(),
+      ninjutsu: Joi.number().required(),
+      genjutsu: Joi.number().required(),
+      endurance: Joi.number().required(),
+      willpower: Joi.number().required(),
+    },
+  }),
+  charactersController.create,
+);
+// appointmentsRouter.get('/me', charactersController.index);
+
+export default charactersRouter;
