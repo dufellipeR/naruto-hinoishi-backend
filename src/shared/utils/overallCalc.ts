6fb51cd50e548f67849a7d51e285a92c8f99f2c6
dufellipeR <eduardo.fellipe30@gmail.com>
2021-01-03 18:40:23 -0300

    OverallCalc

---
+++ src/shared/utils/overallCalc.ts
@@ -0,0 +1,35 @@
+interface IRequestDTO {
+  power: number;
+  intelligence: number;
+  speed: number;
+  taijutsu: number;
+  ninjutsu: number;
+  genjutsu: number;
+  endurance: number;
+  willpower: number;
+}
+
+export default function overallCalc({
+  power,
+  intelligence,
+  speed,
+  taijutsu,
+  ninjutsu,
+  genjutsu,
+  endurance,
+  willpower,
+}: IRequestDTO): number {
+  const total =
+    power +
+    speed +
+    intelligence +
+    taijutsu +
+    ninjutsu +
+    genjutsu +
+    endurance +
+    willpower;
+
+  const overall = total / 8;
+
+  return overall;
+}
