bc63c64a21282e17b6300616587f215bf70119a1
dufellipeR <eduardo.fellipe30@gmail.com>
2021-01-03 18:43:09 -0300

    Adding DTO to Character Creation

---
+++ src/modules/characters/dtos/ICreateCharacterDTO.ts
@@ -0,0 +1,15 @@
+export default interface ICreateCharacterDTO {
+  thumbnail: string;
+  type: string;
+  name: string;
+  desc: string;
+  power: number;
+  intelligence: number;
+  speed: number;
+  taijutsu: number;
+  ninjutsu: number;
+  genjutsu: number;
+  endurance: number;
+  willpower: number;
+  overall: number;
+}
