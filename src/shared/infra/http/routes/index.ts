aa9a0d1dea880cc8690ed100bde72cbed5f1a867
dufellipeR <eduardo.fellipe30@gmail.com>
2021-01-06 09:22:05 -0300

    Added user, sessions, password router to index router

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,14 @@
 import charactersRouter from '@modules/characters/infra/http/routes/characters.routes';
+import passwordRouter from '@modules/user/infra/http/routes/password.routes';
+import sessionsRouter from '@modules/user/infra/http/routes/sessions.routes';
+import usersRouter from '@modules/user/infra/http/routes/users.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/character', charactersRouter);
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
