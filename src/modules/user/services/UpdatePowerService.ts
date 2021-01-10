16a77c3463c02800d82574bdb00ce6cd823c7aab
dufellipeR <eduardo.fellipe30@gmail.com>
2021-01-10 14:28:26 -0300

    Added Update Power Service and tests

---
+++ src/modules/user/services/UpdatePowerService.ts
@@ -0,0 +1,45 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import ICardsRepository from '@modules/cards/repositories/ICardsRepository';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUpdatePowerDTO from '../dtos/IUpdatePowerDTO';
+import User from '../infra/typeorm/entities/User';
+
+interface IRequestDTO {
+  user_id: string;
+}
+
+@injectable()
+export default class UpdatePowerService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('CardsRepository')
+    private cardsRepository: ICardsRepository,
+  ) {}
+
+  public async execute({ user_id }: IRequestDTO): Promise<User | undefined> {
+    const user = this.usersRepository.findById(user_id);
+    console.log(user);
+
+    if (user) {
+      const userCards = await this.cardsRepository.findAllUserCards(user_id);
+      console.log(userCards);
+
+      let power = 0;
+
+      userCards.forEach(card => {
+        power += card.power;
+      });
+
+      console.log(power);
+
+      const updatePower = this.usersRepository.updatePower({ user_id, power });
+
+      return updatePower;
+    }
+    throw new AppError("There's no user with this ID");
+  }
+}
