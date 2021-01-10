e5b43acd975d2491cf3af290f00830d7ada0c500
dufellipeR <eduardo.fellipe30@gmail.com>
2021-01-10 14:31:07 -0300

    Added findAll Method

--- src/modules/user/repositories/IUsersRepository.ts
+++ src/modules/user/repositories/IUsersRepository.ts
@@ -1,10 +1,13 @@
 import User from '../infra/typeorm/entities/User';
 import ICreateUserDTO from '../dtos/ICreateUserDTO';
+import IUpdatePowerDTO from '../dtos/IUpdatePowerDTO';
 
 export default interface IUsersRepository {
+  findAll(): Promise<User[]>;
   findByTag(user_tag: string): Promise<User | undefined>;
   findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
+  updatePower({ power, user_id }: IUpdatePowerDTO): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
   save(user: User): Promise<User>;
 }
