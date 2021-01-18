29c2393d9182a39bcfd147f055b01febb7ad291f
dufellipeR <eduardo.fellipe30@gmail.com>
2021-01-18 17:48:49 -0300

    Registering clan and charclan into routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,7 +3,9 @@ import cardsRouter from '@modules/cards/infra/http/routes/cards.routes';
 import randomCardsRouter from '@modules/cards/infra/http/routes/randomcards.routes';
 import charactersRouter from '@modules/characters/infra/http/routes/characters.routes';
 import charAffiliationRouter from '@modules/char_aft/infra/http/routes/charkekkei.routes';
+import charClanRouter from '@modules/char_clan/infra/http/routes/charclan.routes';
 import charkekkeiRouter from '@modules/char_kg/infra/http/routes/charkekkei.routes';
+import clanRouter from '@modules/clan/infra/http/routes/clan.routes';
 import friendshipRouter from '@modules/friendship/infra/http/routes/friendship.routes';
 import kekkeiRouter from '@modules/kekkei/infra/http/routes/kekkei.routes';
 import passwordRouter from '@modules/user/infra/http/routes/password.routes';
@@ -13,6 +15,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
+routes.use('/clan', clanRouter);
+routes.use('/charclan', charClanRouter);
 routes.use('/affiliation', affiliationRouter);
 routes.use('/charaffiliation', charAffiliationRouter);
 routes.use('/kekkei', kekkeiRouter);
